4c50c728b9523c72e1d0861e84d8820f61bfa523
Boyko Anton <boyko.ant@live.com>
2016-12-04 18:39:41 +0000

    common service

---
+++ app/scripts/services/common.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name carRentalAppApp.common
+ * @description
+ * # common
+ * Service in the carRentalAppApp.
+ */
+angular.module('carRentalAppApp')
+  .service('common', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
