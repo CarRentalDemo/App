70322697a86d0e10f915bac5d1064643df1cc72b
Boyko Anton <boyko.ant@live.com>
2016-12-04 08:41:19 +0000

    empty scafolded app

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name carRentalAppApp
+ * @description
+ * # carRentalAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('carRentalAppApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name carRentalAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the carRentalAppApp
+ */
+angular.module('carRentalAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
