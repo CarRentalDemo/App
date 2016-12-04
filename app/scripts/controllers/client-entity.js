fb16618fa4e259f6053590b85ed3dfa940abeaaa
Boyko Anton <boyko.ant@live.com>
2016-12-04 23:01:51 +0000

    ui for clients

--- app/scripts/controllers/client-entity.js
+++ app/scripts/controllers/client-entity.js
@@ -1,17 +1,30 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name carRentalAppApp.controller:ClientEntityCtrl
- * @description
- * # ClientEntityCtrl
- * Controller of the carRentalAppApp
- */
 angular.module('carRentalAppApp')
-  .controller('ClientEntityCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('ClientEntityCtrl', function ($busy, $scope, $location, $routeParams) {
+    var self = this;
+    self.id = $routeParams.id;
+
+    var odata = o('Clients');
+
+    $scope.model = {};
+    if (self.id) {
+      $busy.during(odata.find(self.id).get()).then(function(response) {
+        $scope.model = response.data;
+        delete $scope.model['@odata.context'];
+        delete $scope.model.Id;
+      });
+    }
+    
+    $scope.saveData = function() {
+      if (self.id) {
+        $busy.during(odata.find(self.id).put($scope.model).save()).then(function(response) {
+          $location.path('/clients-list');
+        })
+      } else {
+        $busy.during(odata.post($scope.model).save()).then(function(response) {
+          $location.path('/clients-list');
+        })
+      }
+    };
   });
--- app/scripts/controllers/clients-list.js
+++ app/scripts/controllers/clients-list.js
@@ -1,17 +1,36 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name carRentalAppApp.controller:ClientsListCtrl
- * @description
- * # ClientsListCtrl
- * Controller of the carRentalAppApp
- */
 angular.module('carRentalAppApp')
-  .controller('ClientsListCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('ClientsListCtrl', function ($scope, $busy, NgTableParams) {
+    var self = this;
+
+    var odata = o('Clients');
+
+    self.tableData = [];
+    self.getData = function() {
+      return self.tableData;
+    };
+
+    $scope.tableParams = new NgTableParams({}, { getData: self.getData });
+
+    $scope.deleteRecord = function(id) {
+      if (!confirm('Are you sure want to delete it?')) {
+        return;
+      }
+
+      $busy.during(odata.find(id).remove().save()).then(function(response){
+        $scope.refreshData();
+      });
+    };
+
+    $scope.refreshData = function() {
+      odata = o('Clients');
+      
+      $busy.during(odata.orderBy('FullName').get()).then(function(response){
+        self.tableData = response.data;
+        $scope.tableParams.reload();
+      });
+    }
+
+    $scope.refreshData();
   });
