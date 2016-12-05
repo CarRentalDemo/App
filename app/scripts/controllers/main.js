1d42889774830444571d5246950d3e8e197dd90a
Boyko Anton <boyko.ant@live.com>
2016-12-05 15:55:14 +0000

    quick actions on main page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,38 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name carRentalAppApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the carRentalAppApp
- */
 angular.module('carRentalAppApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $location, $busy) {
+    $scope.id = null;
+
+    $scope.gotoRentOpen = function(){
+      $busy.during(o('Bookings').select('Id').filter('Id eq ' + $scope.id).get()).then(function(response){
+        var length = response.data.length;
+
+        if (length != 0) {
+          $location.path('/rent-from-booking/' + $scope.id);
+        } else {
+          alert('Booking by this id was not found.');
+        }
+      });
+    };
+
+    $scope.gotoRentClose = function(){
+      $busy.during(o('Bookings').expand('Rents($select=Id,FinalPrice)').select('Rents').filter('Id eq ' + $scope.id).get()).then(function(response){
+        var id = null;
+
+        if (response.data.length != 0) {
+          if (response.data[0].Rents.length != 0) {
+            if (!response.data[0].Rents[0].FinalPrice) {
+              id = response.data[0].Rents[0].Id;
+            }
+          }
+        }
+        
+        if (id) {
+          $location.path('/rent-close-entity/' + id);
+        } else {
+          alert('Booking by this id was not found or it has no open rent.');
+        }
+      });
+    };
   });
